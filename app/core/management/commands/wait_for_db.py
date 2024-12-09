92f08f76b02d8fd53c35f9e9c3828bcca953c16a
Narcis <turaninarcis@gmail.com>
2024-12-09 18:19:45 +0200

    Added authenticating users API with unit tests

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,7 +3,7 @@ Django command to wait for the database to be available.
 """
 from django.core.management.base import BaseCommand
 import time
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error # type: ignore
 from django.db.utils import OperationalError
 
 
